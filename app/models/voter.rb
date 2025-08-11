125b8104698e2111e4f809d72e3be2719ee30dfd
Elise Shaffer <elise@eliseshaffer.com>
2025-08-11 10:32:43 -0600

    Add Voter specs

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,3 @@
 class Voter < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Voter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires an email' do
+      voter = Voter.new(voted_at: Time.current)
+      expect(voter).not_to be_valid
+      expect(voter.errors[:email]).to include("can't be blank")
+    end
+  end
+
+  describe 'uniqueness' do
+    it 'enforces unique email addresses' do
+      Voter.create!(email: 'user@example.com', voted_at: Time.current)
+      
+      duplicate_voter = Voter.new(email: 'user@example.com', voted_at: Time.current)
+      expect(duplicate_voter).not_to be_valid
+      expect(duplicate_voter.errors[:email]).to include('has already been taken')
+    end
+
+    it 'prevents duplicate emails at the database level' do
+      Voter.create!(email: 'user@example.com', voted_at: Time.current)
+      
+      expect {
+        Voter.create!(email: 'user@example.com', voted_at: Time.current)
+      }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
 end
